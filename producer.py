7f94dcdb3094774a977782a027ad534e7044136f
vivianh1234 <56896686+vivianh1234@users.noreply.github.com>
2021-01-22 11:32:49 -0800

    Create producer.py

---
+++ producer.py
@@ -0,0 +1,42 @@
+
+    # Create Producer instance
+    producer = Producer({
+        'bootstrap.servers': conf['bootstrap.servers'],
+        'sasl.mechanisms': conf['sasl.mechanisms'],
+        'security.protocol': conf['security.protocol'],
+        'sasl.username': conf['sasl.username'],
+        'sasl.password': conf['sasl.password'],
+    })
+
+    # Create topic if needed
+    ccloud_lib.create_topic(conf, topic)
+
+    delivered_records = 0
+
+    # Optional per-message on_delivery handler (triggered by poll() or flush())
+    # when a message has been successfully delivered or
+    # permanently failed delivery (after retries).
+    def acked(err, msg):
+        global delivered_records
+        """Delivery report handler called on
+        successful or failed delivery of message
+        """
+        if err is not None:
+            print("Failed to deliver message: {}".format(err))
+        else:
+            delivered_records += 1
+            print("Produced record to topic {} partition [{}] @ offset {}"
+                  .format(msg.topic(), msg.partition(), msg.offset()))
+
+    for n in range(100):
+        record_key = "alice"
+        record_value = json.dumps({'count': n})
+        print("Producing record: {}\t{}".format(record_key, record_value))
+        producer.produce(topic, key=record_key, value=record_value, on_delivery=acked)
+        # p.poll() serves delivery reports (on_delivery)
+        # from previous produce() calls.
+        producer.poll(0)
+
+    producer.flush()
+
+    print("{} messages were produced to topic {}!".format(delivered_records, topic))
